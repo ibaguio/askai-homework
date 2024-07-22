41f8f524ed885c0c572fbca056f1ae75440b05d2
ibaguio <baguio.ivan@gmail.com>
2024-07-22 09:53:39 +0200

    Add basic frontend react application

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from services import InterferenceRunnerService
 from services import ChunkHolderService
 
@@ -7,28 +9,35 @@ app = FastAPI()
 irs = InterferenceRunnerService()
 chs = ChunkHolderService()
 
+origins = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+)
 
 @app.get("/ask")
 async def ask(q: str):
     # Note: hardcoded for easier testing
     # TODO: remove this
+    # q = "How do I remove a teammate?"
 
-    q = "How do I remove a teammate?"
     chunks = irs.ask_question(q)
     chunk_contents = chs.get_chunk_contents(chunks)
 
     chunks_data = {
-        chunk["chunkId"]: chunk["confidence"]
+        chunk["chunkId"]: {
+            "confidence": chunk["confidence"]
+        }
         for chunk in chunks
     }
 
     # merge content and confidence level
     for uuid, content in chunk_contents.items():
-        chunks_data[uuid] = content
+        chunks_data[uuid]["content"] = content
 
     return chunks_data
--- react-app-js/src/App.js
+++ react-app-js/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    const endpoint = `http://localhost:8000/ask?q=${query}`;
+
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => {
+        const parsedData = Object.entries(data).map(([id, values]) => ({ id, ...values }));
+        setResults(parsedData)
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ask me anything!</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Search..."
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div>
+        {results.map((item, index) => (
+          <div key={index} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
+            <div dangerouslySetInnerHTML={{ __html: item.content }} />
+            <div><strong>Confidence</strong>: {item.confidence.toFixed(2)}% </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
